e2fb40c579c499e919e330ae6382e3ac722246c0
FernandoGP06 <halo_fercho1@hotmail.com>
2020-10-22 14:32:39 -0500

    actualizacion vol3

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,9 +1,10 @@
 <?php
 $result="";
 if(isset($_POST['submit'])){
- if(!empty($_POST['name']) && !empty($_POST['subject']) && !empty($_POST['message']) && !empty($_POST['email']))
+if(!empty($_POST['name']) && !empty($_POST['subject']) && !empty($_POST['message']) && !empty($_POST['email']))
 require 'phpmailer/PHPMailerAutoload.php'
-$mail = new phpmailer;
+$mail = new PHPMailer;
+$mail = ->isSMTP();
 $mail->Host='smtp.gmail.com';
 $mail->Port=587;
 $mail->SMTPAuth=true;
@@ -11,23 +12,18 @@ $mail->SMTPSecure='tls';
 $mail->Username='guicodecodigo@gmail.com';
 $mail->Password='guicode157';
 
-$mail->setFrom($_POST['email'],
- $_POST['name'],
- $_POST['subject'],
- $_POST['message']);
-
- $mail->addAddress('galindoperezfernando@gmail.com');
- $mail->addReplyTo($_POST['email'],$_POST['name']);
+$mail->setFrom($_POST['email'],$_POST['name']);
+$mail->addAddress('galindoperezfernando@gmail.com');
+$mail->addReplyTo($_POST['email'],$_POST['name']);
  
  $mail->isHTML(true);
- $mail->Subjet='Enviado por '.$_POST['name'];
-
+ $mail->Subject='Enviado por '.$_POST['name'];
  $mail->Body='<h1 align=center>name: '.$_POST['name'] .'<br>email: '.$_POST['email'].'<br>subject: '.$_POST['subject']
  .'<br>message: '.$_POST['message'].'</h1>';
 
 //$mail = @mail($destino,$correo,$name,$asunto,$mensaje);
 
-if($mail->send){
+if(!$mail->send){
         $result="OK";
     }
 }
